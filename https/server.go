ea4f2266d301506e9eb0c7d939779c27f7a8376a
王健 <wangjian41@100.me>
2023-01-11 14:04:00 +0800

    新增https

---
+++ https/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type textHandler struct {
+	responseText string
+}
+
+func (th *textHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, th.responseText)
+}
+
+type indexHandler struct{}
+
+func (ih *indexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	html := `<doctype html>
+        <html>
+        <head>
+          <title>Hello World</title>
+        </head>
+        <body>
+        <p>
+          <a href="/welcome">Welcome</a> |  <a href="/message">Message</a>
+        </p>
+        </body>
+</html>`
+	fmt.Fprintln(w, html)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", &indexHandler{})
+
+	thWelcome := &textHandler{"TextHandler !"}
+	mux.Handle("/text", thWelcome)
+
+	http.ListenAndServe(":8084", mux)
+}
